4bfb055612a3f066ad59d38ca63199f48eabd5fb
Edmond GAKS <edmondgaks@gmail.com>
2022-06-11 11:22:12 +0200

    adding sucessfully our navbar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,14 +7,19 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../context/ContextProvider';
 
 const Sidebar = () => {
-    const {activeMenu,setActiveMenu} = useStateContext();
+    const {activeMenu,setActiveMenu,screenSize} = useStateContext();
+    const handleCloseSidebar = () => {
+        if(activeMenu && screenSize <= 900) {
+            setActiveMenu(false);
+        }
+    }
     const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
     const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
   return (
     <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
         {activeMenu && (<>
             <div className='flex justify-between items-center'>
-                <Link to="/" onClick={() => setActiveMenu(false)} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+                <Link to="/" onClick={() => handleCloseSidebar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
                     <SiShopware className='text-3xl' /> <span>E-DASH</span>
                 </Link>
                 <TooltipComponent content="Menu" position="BottomCenter">
@@ -30,7 +35,7 @@ const Sidebar = () => {
                         {item.title}
                         </p>
                         {item.links.map((link) => (
-                            <NavLink to={`/${link.name}`} key={link.name} onClick={() =>  {}} className={({ isActive }) => isActive ? activeLink : normalLink}>
+                            <NavLink to={`/${link.name}`} key={link.name} onClick={handleCloseSidebar} className={({ isActive }) => isActive ? activeLink : normalLink}>
                                 {link.icon}
                                 <span className='capitalize'>{link.name}</span>
                             </NavLink>
--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -11,10 +11,20 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [activeMenu,setActiveMenu] = useState(true);
+    const [isClicked,setisClicked] = useState(initialState);
+    const handleClick = (clicked) => {
+        setisClicked({...initialState, [clicked]: true });
+    }
+    const [screenSize,setScreenSize] = useState(undefined);
     return (
         <StateContext.Provider value={{
             activeMenu,
             setActiveMenu,
+            isClicked,
+            setisClicked,
+            handleClick,
+            screenSize,
+            setScreenSize
             }}>
             {children}
         </StateContext.Provider>
