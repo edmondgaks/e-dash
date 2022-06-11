6fca35b2a5814a30bd0440b1dc3869d7cca4f325
Edmond GAKS <edmondgaks@gmail.com>
2022-06-11 10:30:15 +0200

    adding a tooltip component to contain all our user infoo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect} from 'react'
 import { AiOutlineMenu } from 'react-icons/ai';
 import { FiBarChart, FiShoppingCart } from 'react-icons/fi';
 import { BsChatLeft } from 'react-icons/bs';
-import { RiNotification2Line } from 'react-icons/ri';
+import { RiNotification2Line, RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import avatar from '../data/avatar.jpg';
@@ -25,6 +25,13 @@ const Navbar = () => {
       <div className='flex'>
       <NavButton title="Cart" customFunc={() => setActiveMenu(() => handleClick('cart'))} color="blue" icon={<FiShoppingCart />} />
       <NavButton title="Chat" dotColor="#03C9D7" customFunc={() => setActiveMenu(() => handleClick('chat'))} color="blue" icon={<BsChatLeft />} />
+      <NavButton title="Notification" customFunc={() => setActiveMenu(() => handleClick('notification'))} color="blue" icon={<RiNotification3Line />} />
+      <TooltipComponent content='Profile' position='BottomCenter'>
+        <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg' onClick={() => handleClick('userProfile')}>
+          
+        </div>
+      </TooltipComponent>
+
       </div>
     </div>
   )
