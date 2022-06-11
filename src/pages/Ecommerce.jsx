06ff073f15bceb4283bdfa847639a90538b46cfd
Edmond GAKS <edmondgaks@gmail.com>
2022-06-11 12:07:15 +0200

    importing all to use

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { BsCurrencyDollar } from 'react-icons/bs';
+import { GoPrimitiveDot } from 'react-icons/go';
+import { Stacked,Pie,Button,Sparkline } from '../components';
+import { earningData,SparklineAreaData,ecomPieChartDat } from '../data/dummy';
+import { useStateContext } from '../context/ContextProvider';
+
 
 const Ecommerce = () => {
   return (
