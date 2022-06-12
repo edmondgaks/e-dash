0a0dfbbb227a8bfd586c3f867806d0b73b422cc0
Edmond GAKS <edmondgaks@gmail.com>
2022-06-12 10:11:05 +0200

    adding all components

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -46,6 +46,21 @@ const Ecommerce = () => {
                 <span><GoPrimitiveDot /></span>
                 <span>Expenses</span>
               </p>
+              <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'>
+                <span><GoPrimitiveDot /></span>
+                <span>Budget</span>
+              </p>
+            </div> 
+          </div>
+          <div className="mt-10 flex gap-10 flex-wrap justify-center">
+            <div className="border-r-1 border-color m-4 pr-10">
+              <div>
+                <p>
+                  <span className="text-3xl font-semibold">$93,438</span>
+                  <span className="p-1.5 hover:drop-shadow-xl cursor-pointer bg-green-400 ml-3 text-xs rounded-full text-white ">23%</span>
+                </p>
+                <p className="text-gray-500 mt-1">Budget</p>
+              </div>
             </div>
           </div>
         </div>
