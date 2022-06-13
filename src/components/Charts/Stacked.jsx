1e5d1fadec51bc64137fd94d399db4ff466ae0fa
Edmond GAKS <edmondgaks@gmail.com>
2022-06-13 07:53:13 +0200

    adding the user's chart

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -4,8 +4,11 @@ import { stackedCustomSeries,stackedPrimaryXAxis, stackedPrimaryYAxis} from '../
 
 const Stacked = ({width,height}) => {
   return (
-    <ChartComponent width={width} height={height} id=''>
+    <ChartComponent width={width} height={height} id='stack chart' primaryXAxis={stackedPrimaryXAxis} primaryYAxis={stackedPrimaryYAxis} chartArea={{border: {width: 0}}} tooltip={{enable: true}} legendSettings={{background: 'white'}}>
       <Inject services={[Legend,Category,StackingColumnSeries,Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item,index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
     </ChartComponent>
   )
 }
