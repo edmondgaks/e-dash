57370464ed9c3de1ed64ccf9c6362d86b6d3de6e
Edmond GAKS <edmondgaks@gmail.com>
2022-06-14 11:55:30 +0200

    finalising the calendar and kanban pages

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -6,10 +6,11 @@ import { Header } from '../components';
 const Kanban = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <Header category="App" title="Calendar" />
+      <Header category="App" title="Kanban" />
       <KanbanComponent>
-        <ColumnsDirective>
-        {/* {kanbanGrid.map((item,index) => )} */}
+        <ColumnsDirective id="kanban" dataSource={kanbanData} cardSettings={{ contentField: 'Summary', headerField: 'Id' }} keyField="Status">
+        {kanbanGrid.map((item,index) => 
+        <ColumnDirective key={index} {...item} />)}
         </ColumnsDirective>
       </KanbanComponent>
     </div>
