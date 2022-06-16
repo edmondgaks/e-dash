e85cc3156bb4f33e2c15032c5f7cb4f6a8321864
Edmond GAKS <edmondgaks@gmail.com>
2022-06-16 21:35:33 +0200

    adding the color picker successfully

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React from 'react';
 import { ColorPickerComponent } from '@syncfusion/ej2-react-inputs';
+import { Header } from '../components';
+
 const change = (args) => {
   document.getElementById('preview').style.backgroundColor = args.currentValue.hex;
 }
