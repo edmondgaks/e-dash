48205600cd516b3f62d392b59a570c2bee8469a6
Edmond GAKS <edmondgaks@gmail.com>
2022-06-16 21:30:30 +0200

    Fixing the error and rendering the colorpicker page

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -3,7 +3,7 @@ import { MdOutlineCancel } from 'react-icons/md';
 import { BsCheck } from 'react-icons/bs';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { themeColors } from '../data/dummy';
-import { useStateContext } from '../contexts/ContextProvider';
+import { useStateContext } from '../context/ContextProvider';
 const ThemeSettings = () => {
   return (
     <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0 ">
