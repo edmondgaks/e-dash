25c32073fd5d5629fd793e01d160f288956b071e
Edmond GAKS <edmondgaks@gmail.com>
2022-07-31 14:32:43 +0200

    adding the chart component

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import { ChartComponent,SeriesCollectiveDirective,SeriesDirective,LineSeries,Inject,DateTime,Legend,Tooltip } from '@syncfusion/ej2-react-charts';
+import { lineCustomSeries,LinePrimaryYAxis,LinePrimaryXAxis} from '../../data/dummy';
 const LineChart = () => {
   return (
-    <div>LineChart</div>
+    <div>
+      <ChartComponent>
+        <Inject services={[LineSeries,DateTime,Legend,Tooltip]} />
+      </ChartComponent>
+    </div>
   )
 }
 
