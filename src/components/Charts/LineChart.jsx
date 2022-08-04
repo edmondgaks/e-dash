f748172bd37845f854d992518d4c5834cf23470e
Edmond GAKS <edmondgaks@gmail.com>
2022-08-04 22:19:09 +0200

    adding some changes

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { ChartComponent,SeriesCollectiveDirective,SeriesDirective,LineSeries,Inject,DateTime,Legend,Tooltip } from '@syncfusion/ej2-react-charts';
-import { lineCustomSeries,LinePrimaryYAxis,LinePrimaryXAxis} from '../../data/dummy';
+
 const LineChart = () => {
   return (
     <div>
       <ChartComponent>
         <Inject services={[LineSeries,DateTime,Legend,Tooltip]} />
+        <SeriesCollectionDirective>
+          {lineCustomSeries.map((item,index) => <SeriesDirective key={index} {...item} />)}
+        </SeriesCollectionDirective>
       </ChartComponent>
     </div>
   )
 }
+
 
 export default LineChart
